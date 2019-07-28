78010a72e5c63194bdbb5657628e6abea376ec1d
brok1n <brok1n@outlook.com>
2019-07-28 18:58:35 +0800

    create project

---
+++ filemanager/src/main/kotlin/com/brok1n/web/filemanager/FilemanagerApplication.kt
@@ -0,0 +1,11 @@
+package com.brok1n.web.filemanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FilemanagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FilemanagerApplication>(*args)
+}
